4387168e5fce62dcaac3d41481baa29f9ee5e920
yml <@yml>
2023-02-24 14:21:51 +0800

    modify

--- proxy.bat
+++ proxy.bat
@@ -106,7 +106,7 @@ REM reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Co
 
 echo 正在设置代理服务器……
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:51168" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:8689" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;172.32.*;192.168.*;<local>" /f
 
 rem “对于本地地址不使用代理服务器”这个勾，不会勾选上
